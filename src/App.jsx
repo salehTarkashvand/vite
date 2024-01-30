5d1bace4cf537682970e25c395edc3186543fc82
ewdwins <saleh.tarkashvand@gmail.com>
2024-01-30 11:35:49 +0330

    “add-tailwind”

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React ,{useState , useEffect, useRef} from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue,setInputValue]=useState('')
+  const [users,setUsers] =useState([
+    {id:1 , name:'saleh',age:24 , make:'programming'},
+    {id:2 ,name:'hamed',age:19 , make:'programming'},
+    {id:3 ,name:'mahmod',age:20 , make:'programming'},
+    {id:4 ,name:'maryam',age:18 , make:'programming'}
+  ])
+  useEffect(()=>{
+    console.log("mount");
+  return(()=>{
+    console.log("update");
+  })
+  },[users])
 
+  const inputRef = useRef(null)
+
+  const User = ({id , name , age , make})=>{
+   return(
+    <li onClick={change}>
+    {id}-{name}-{age}-{make}
+    </li>
+   )
+  }
+  
+  const changeInputValue = ({target})=>{
+    const resultValue = target.value;
+    setInputValue(resultValue)
+
+  }
+  const handleKeyPress =(e)=>{
+   if(e.keyCode === 13 ){
+    addItem()
+   }
+  }
+  const addItem = ()=>{
+    // if(!inputValue) return
+    
+    setUsers([
+      ...users,
+      {id:users.length+1 , name:inputValue , age:20 , make:"programing"}
+    ])
+    console.log(users);
+    setInputValue('')
+    inputRef.current.focus()
+
+  } 
+  
+  
+  const change = ()=>{
+    setUsers([...users , {id:5 ,name:'maman',age:18 , make:'programming'} ])
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div>
+    <input value={inputValue} onChange={changeInputValue} onKeyUp={handleKeyPress} ref={inputRef} ></input>
+    <button onClick={addItem} disabled={!(inputValue)} >
+      add
+    </button>
+    <ul>
+      {users.map(({id , name , age , make})=>{
+       return (
+      //  <li key={id} onClick={change}>
+      //   {id}-{name}-{age}-{make}
+      //   </li>
+       
+       <User key={id} id={id} onClick={change} name={name} age={age} make={make} />
+       )
+      })}
+    </ul>
+  </div>
+  
   )
 }
 
